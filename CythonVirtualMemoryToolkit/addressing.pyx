718bbc1164b417f689e910896ef21352682208e9
Leon Bass <leonandersonbass@gmail.com>
2024-03-23 01:39:10 +0000

    undo previous

--- CythonVirtualMemoryToolkit/addressing.pyx
+++
@@ -1,9 +0,0 @@
-
-
-cdef extern from "Windows.h":
-    BOOL ReadProcessMemory(HANDLE hProcess, LPCVOID lpBaseAddress, LPVOID lpBuffer, SIZE_T nSize, SIZE_T* lpNumberOfBytesRead)
-    
-
-cdef class Address:
-
-    
