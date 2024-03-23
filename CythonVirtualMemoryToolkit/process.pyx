e32ee1b2937083119622d16db0dae1ae06226efb
Leon Bass <leonandersonbass@gmail.com>
2024-03-23 01:38:37 +0000

    added read and write process memeory. For future can compile the windows wrpped functions in own module then just use them inside of my cython project as pre-compiled module

--- CythonVirtualMemoryToolkit/process.pyx
+++ CythonVirtualMemoryToolkit/process.pyx
@@ -18,16 +18,47 @@ cdef extern from "Windows.h":
     HWND FindWindowA(LPCSTR lpClassName, LPCSTR lpWindowName)
     int GetWindowThreadProcessId(HWND hWnd, DWORD* lpdwProcessId)
     int CloseHandle(HANDLE handle)
-    BOOL ReadProcessMemory(HANDLE hProcess, LPCVOID lpBaseAddress, LPVOID lpBuffer, SIZE_T nSize, SIZE_T* lpNumberOfBytesRead)
-    
+    BOOL ReadProcessMemory(
+        HANDLE hProcess, 
+        LPCVOID lpBaseAddress, 
+        LPVOID lpBuffer, 
+        SIZE_T nSize, 
+        SIZE_T* out_lpNumberOfBytesRead
+    )
+    BOOL WriteProcessMemory(
+        HANDLE  hProcess,
+        LPVOID  lpBaseAddress,
+        LPCVOID lpBuffer,
+        SIZE_T  nSize,
+        SIZE_T  *lpNumberOfBytesWritten
+    )
 
 cdef HANDLE get_process_handle(const char* windowName) :
-
     cdef HWND gameWindow = FindWindowA(<LPCSTR>0, windowName)
     cdef DWORD gameProcessId = 0
     GetWindowThreadProcessId(gameWindow, &gameProcessId)
     CloseHandle(gameWindow)
     return OpenProcess(PROCESS_ALL_ACCESS, False, gameProcessId)
+
+cdef int read_process_memory(
+    HANDLE process_handle, 
+    LPCVOID base_address,
+    LPVOID read_buffer, 
+    SIZE_T number_of_bytes
+):
+    cdef SIZE_T read_bytes = 0
+    ReadProcessMemory(process_handle, base_address, read_buffer, number_of_bytes, &read_bytes)
+    return read_bytes
+
+cdef int write_process_memory(
+    HANDLE process_handle,
+    LPVOID base_address,
+    LPCVOID write_buffer,
+    SIZE_T number_of_bytes
+):
+    cdef SIZE_T written_bytes = 0
+    WriteProcessMemory(process_handle, base_address, write_buffer, number_of_bytes, &written_bytes)
+    return written_bytes
 
 cdef class WindowHandle:
     cdef HANDLE _process_handle
