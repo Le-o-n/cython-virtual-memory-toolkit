f29a735e842f2f2002f83790ae2fc01e534aa7cc
Leon Bass <leonandersonbass@gmail.com>
2024-03-24 01:55:42 +0000

    Working reading and writing bytes to the process's memory using maximum privilages.

--- CythonVirtualMemoryToolkit/process.pyx
+++ CythonVirtualMemoryToolkit/process.pyx
@@ -1,6 +1,7 @@
 from libc.stdlib cimport malloc, free
+from libc.stdint cimport uint8_t
+from cpython cimport array
 
-cdef unsigned long PROCESS_ALL_ACCESS = 0x001FFFFF
 
 cdef extern from "Windows.h":
     ctypedef unsigned long DWORD
@@ -18,6 +19,8 @@ cdef extern from "Windows.h":
     ctypedef int BOOL
     ctypedef BOOL (*WNDENUMPROC)(HWND hWnd, LPARAM lParam)
 
+    cdef unsigned long PROCESS_ALL_ACCESS
+
     HANDLE OpenProcess(DWORD dwDesiredAccess, BOOL bInheritHandle, DWORD dwProcessId)
     HWND FindWindowA(LPCSTR lpClassName, LPCSTR lpWindowName)
     int GetWindowThreadProcessId(HWND hWnd, DWORD* lpdwProcessId)
@@ -28,33 +31,50 @@ cdef extern from "Windows.h":
     int GetWindowTextA(HWND  hWnd, LPSTR out_lpString, int nMaxCount)
     BOOL EnumWindows(WNDENUMPROC lpEnumFunc, LPARAM lParam)
     BOOL IsWindowVisible(HWND hWnd)
+    DWORD GetLastError()
 
 cdef struct EnumWindowCallbackLParam:
-    char* window_name
-    HANDLE out_handle
-    int out_matches
+    char* in_window_name
+    HWND out_window_handle
+    DWORD out_pid
+    HANDLE out_all_access_process_handle
+    char* out_full_window_name
 
 cdef BOOL enum_window_match_callback(HWND hWnd, LPARAM lparam) noexcept:
     cdef EnumWindowCallbackLParam* data = <EnumWindowCallbackLParam*>lparam
     cdef int length = GetWindowTextLengthA(hWnd)
     cdef char* text_buffer = <char*>malloc(sizeof(char) * (length + 1))
+    cdef DWORD target_pid = 0
     GetWindowTextA(hWnd, text_buffer, length + 1);
     
     if (length != 0 and IsWindowVisible(hWnd)):
-        if data.window_name in text_buffer:
-            data.out_handle = hWnd
-            data.out_matches += 1
-
+        if data.in_window_name in text_buffer:
+            GetWindowThreadProcessId(hWnd, &target_pid)
+            data.out_pid = target_pid
+            data.out_window_handle = hWnd
+            data.out_all_access_process_handle = OpenProcess(
+                PROCESS_ALL_ACCESS,
+                False,
+                target_pid
+            )
+            data.out_full_window_name = text_buffer
+            return False
+    
     free(text_buffer)
+    return True
 
-    return True;
-
-cdef HANDLE find_process(char* window_name):
+cdef EnumWindowCallbackLParam find_process(char* window_name):
     cdef EnumWindowCallbackLParam data
-    data.window_name = window_name
-    data.out_matches = 0
-    EnumWindows(enum_window_match_callback, <LPARAM>&data);
-    return data.out_handle
+
+
+    data.in_window_name = window_name
+    data.out_all_access_process_handle = 0
+    data.out_pid = 0
+    data.out_window_handle = 0
+    EnumWindows(enum_window_match_callback, <LPARAM>&data)
+
+    
+    return data
 
 cdef HANDLE get_process_handle_from_window_name(const char* windowName) :
     cdef HWND gameWindow = FindWindowA(<LPCSTR>0, windowName)
@@ -62,6 +82,8 @@ cdef HANDLE get_process_handle_from_window_name(const char* windowName) :
     GetWindowThreadProcessId(gameWindow, &gameProcessId)
     CloseHandle(gameWindow)
     return OpenProcess(PROCESS_ALL_ACCESS, False, gameProcessId)
+
+
 
 cdef int read_process_memory(
     HANDLE process_handle, 
@@ -95,20 +117,109 @@ cdef int write_process_memory(
     )
     return written_bytes
 
-cdef class WindowHandle:
+cdef class ApplicationHandle:
     cdef HANDLE _process_handle
-    cdef const char* window_name
+    cdef HWND _window_handle
+    cdef const char* _window_name
+    cdef DWORD _pid
+    cdef bint is_verbose
 
-    def __cinit__(self, const char* window_name):
-        self.window_name = window_name
-        self._process_handle = find_process(window_name)
+    def __cinit__(self, const char* window_name, bint is_verbose = False):
+        
+
+        cdef EnumWindowCallbackLParam window_data = find_process(window_name)
+        cdef unsigned long error_code
+        self._process_handle = window_data.out_all_access_process_handle
+        self._window_handle = window_data.out_window_handle
+        self._window_name = window_data.out_full_window_name
+        self._pid = window_data.out_pid
+        self.is_verbose = is_verbose
+
+        if is_verbose:
+            print("=======================================")
+            print(" Window name    = ", self._window_name)
+            print(" Process handle = ", self._process_handle)
+            print(" Window handle  = ", self._window_handle)
+            print(" PID            = ", self._pid)
+            print("=======================================")
+
         if self._process_handle == 0:
-            raise RuntimeWarning("Unable to get handle to process...")
+            error_code = GetLastError()  
+            if error_code == 5:
+                raise RuntimeError("Unable to get a privilaged handle to target process, please re-run using administrator :)")  
+            raise RuntimeError("Unable to get a privilaged handle to target process, unknown error. Error code: " + str(error_code) + ". You can find the reason for the error by querying the error code here: https://learn.microsoft.com/en-us/windows/win32/debug/system-error-codes")
 
     def __dealloc__(self):
         CloseHandle(self._process_handle)
         self._process_handle = 0
+
+        free(self._window_name)
         
 
-    def get_handle(self) -> int:
+    def read_memory_bytes(
+        self, 
+        unsigned long address, 
+        int bytes_to_read
+    ) -> bytes:
+        
+        cdef char* read_buffer
+        cdef SIZE_T num_bytes_read 
+        cdef bytes py_memory
+
+        read_buffer = <char*>malloc(bytes_to_read)
+        
+        if not read_buffer:
+            raise MemoryError("Failed to allocate memory.")
+
+        num_bytes_read = read_process_memory(self._process_handle, <LPCVOID>address, <LPVOID>read_buffer, bytes_to_read)
+
+        if num_bytes_read != bytes_to_read:
+            raise MemoryError(f"Error reading memory. Read bytes: {num_bytes_read}. Bytes instructed to read: {bytes_to_read}.")
+
+        py_memory = bytes(<char[:bytes_to_read]>read_buffer)
+        
+        free(read_buffer)
+
+        return py_memory
+
+    def write_memory_bytes(
+        self, 
+        unsigned long address, 
+        bytes bytes_to_write
+    ) -> None:
+        
+        cdef char* write_buffer
+        cdef SIZE_T num_bytes_written 
+
+        write_buffer = <char*>malloc(sizeof(char) * len(bytes_to_write))
+
+        for i in range(len(bytes_to_write)):
+            write_buffer[i] = bytes_to_write[i]
+
+        if not write_buffer:
+            raise MemoryError("Failed to allocate memory.")
+
+        num_bytes_written = write_process_memory(self._process_handle, <LPCVOID>address, <LPCVOID>write_buffer, len(bytes_to_write))
+
+        if num_bytes_written != len(bytes_to_write):
+            raise MemoryError(f"Error writing to memory. Written bytes: {num_bytes_written}. Bytes instructed to write: {len(bytes_to_write)}.")
+
+        
+        free(write_buffer)
+
+        return
+
+    
+
+    @property
+    def window_handle(self) -> int:
+        return self._window_handle
+    
+    @property
+    def process_handle(self) -> int:
         return self._process_handle
+
+    @property
+    def pid(self) -> int:
+        return self._pid    
+
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from CythonVirtualMemoryToolkit import process
 
 
-window = process.WindowHandle(b"Step")
+app = process.ApplicationHandle(b"Step", True)
+x = app.read_memory_bytes(0x000ED498, 2)  # Read 1 byte from memory
+bytes_as_ints = list(x)  # Convert each byte in the bytes object to an int
+print(bytes_as_ints)
+
+app.write_memory_bytes(0x000ED498, bytes([69,69]))
+
+x = app.read_memory_bytes(0x000ED498, 2)  # Read 1 byte from memory
+bytes_as_ints = list(x)  # Convert each byte in the bytes object to an int
+print(bytes_as_ints)
+
